321368aaa20746b4ba2843efdd3a89042de62472
Kenne <kenneth.langedal@hotmail.com>
2018-09-28 12:33:05 +0200

    noe'

--- Auction/src/main/java/ejb/StartupBean.java
+++ Auction/src/main/java/ejb/StartupBean.java
@@ -10,6 +10,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import entities.Bid;
 import entities.Product;
 import entities.User;
 
@@ -22,46 +23,59 @@ public class StartupBean {
 	
     @PostConstruct
     public void init() {
-//    	TypedQuery<User> deleteUsers = em.createQuery("DELETE FROM User CASCADE", User.class);
-//    	deleteUsers.executeUpdate();
-//    	
-//    	TypedQuery<Product> deleteProducts = em.createQuery("DELETE FROM Product CASCADE", Product.class);
-//    	deleteProducts.executeUpdate();
+    	TypedQuery<User> deleteUsers = em.createQuery("DELETE FROM User CASCADE", User.class);
+    	deleteUsers.executeUpdate();
     	
-//    	User user = new User();
-//    	user.setfName("Kenneth nr1");
-//    	user.setlName("Langedal");
-//    	user.setPhone(90682290);
-//    	user.setEmail("Kenneth.Langedal@hotmail.com");
-////    	
-//    	Product p1 = new Product();
-//    	p1.setActive(false);
-//    	p1.setName("Fersk sjokolademelk");
-//    	p1.setEndTime(new Date(10000));
-//    	p1.setUser(user);
-//    	user.setProducts(new ArrayList<Product>() {{add(p1);}});
-//    	
-//    	User user1 = new User();
-//    	user1.setfName("Markus");
-//    	user1.setlName("Løland");
-//    	user1.setPhone(123456789);
-//    	user1.setEmail("Markus.Løland@hotmail.com");
+    	TypedQuery<Product> deleteProducts = em.createQuery("DELETE FROM Product CASCADE", Product.class);
+    	deleteProducts.executeUpdate();
     	
-//    	Product p2 = new Product();
-//    	p2.setActive(false);
-//    	p2.setName("Coca Cola Lemon");
-//    	p2.setEndTime(new Date(111111));
-//    	p2.setUser(user);
-//    	user.setProducts(new ArrayList<Product>() {{add(p2);}});
-//    	
-//    	Product p3 = new Product();
-//    	p3.setUser(user);
-//    	user.setProducts(new ArrayList<Product>() {{add(p3);}});
-//    	
-//    	em.persist(user);
-//    	em.persist(user1);
-//    	em.persist(p3);
-//    	em.persist(p1);
-//    	em.persist(p2);
+    	User user = new User();
+    	user.setfName("Kenneth nr1");
+    	user.setlName("Langedal");
+    	user.setPhone(90682290);
+    	user.setEmail("Kenneth.Langedal@hotmail.com");
+
+    	Product p1 = new Product();
+    	p1.setActive(false);
+    	p1.setName("Fersk sjokolademelk");
+    	p1.setEndTime(new Date(10000));
+    	p1.setUser(user);
+    	
+    	user.setProducts(new ArrayList<Product>() {{add(p1);}});
+    	
+    	User user1 = new User();
+    	user1.setfName("Markus");
+    	user1.setlName("Løland");
+    	user1.setPhone(123456789);
+    	user1.setEmail("Markus.Løland@hotmail.com");
+    	
+    	Bid bid = new Bid();
+    	bid.setUser(user1);
+    	bid.setAmount(200);
+    	
+    	Bid bid2 = new Bid();
+    	bid2.setUser(user1);
+    	bid2.setAmount(200);
+    	
+    	p1.setBidHistory(new ArrayList<Bid>() {{add(bid); add(bid2);}});
+    	
+    	Product p2 = new Product();
+    	p2.setActive(false);
+    	p2.setName("Coca Cola Lemon");
+    	p2.setEndTime(new Date(111111));
+    	p2.setUser(user);
+    	user.setProducts(new ArrayList<Product>() {{add(p2);}});
+    	
+    	Product p3 = new Product();
+    	p3.setUser(user);
+    	user.setProducts(new ArrayList<Product>() {{add(p3);}});
+    	
+    	em.persist(user);
+    	em.persist(user1);
+    	em.persist(p3);
+    	em.persist(p1);
+    	em.persist(p2);
+    	em.persist(bid);
+    	em.persist(bid2);
     }
 }
--- Auction/src/main/java/rest/RestService.java
+++ Auction/src/main/java/rest/RestService.java
@@ -6,11 +6,14 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.websocket.server.PathParam;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import entities.Bid;
 import entities.Product;
@@ -22,33 +25,37 @@ import entities.Product;
 @Path("/auctions")
 @Stateless
 public class RestService {
-	
+
 	@PersistenceContext(unitName = "Auction")
 	private EntityManager em;
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public List<Product> getProducts() {
+	public List<Product> getAuctions() {
 		TypedQuery<Product> query = em.createNamedQuery(Product.FIND_ALL, Product.class);
 		return query.getResultList();
 	}
-	
+
 	@GET
+	@Produces(MediaType.APPLICATION_XML)
 	@Path("{id}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getProduct(@PathParam("id") String id) {
-		//int idp = Integer.parseInt(id);
-		//Product prod = em.find(Product.class, idp);
-		//return prod;
-		return id;
+	public Product getAuction(@PathParam("id") String id) {
+		int idInt = Integer.parseInt(id);
+		Product product = em.find(Product.class, idInt);
+		if (product == null)
+			throw new NotFoundException();
+		return product;
 	}
 	
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	@Path("bid")
-	public List<Bid> getBids() {
-		TypedQuery<Bid> query = em.createNamedQuery(Bid.FIND_ALL, Bid.class);
-		return query.getResultList();
+	@Path("{id}/bids")
+	public List<Bid> getBids(@PathParam("id") String id) {
+		int idInt = Integer.parseInt(id);
+		Product product = em.find(Product.class, idInt);
+		if (product == null)
+			throw new NotFoundException();
+		return product.getBidHistory();
 	}
 }
 

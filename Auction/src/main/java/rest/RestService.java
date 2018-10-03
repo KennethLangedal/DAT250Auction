2675f7cbf7060b2ec8051a087f915ffba599d7c7
Kenne <kenneth.langedal@hotmail.com>
2018-10-03 14:03:13 +0200

    Ordnet rest service

--- Auction/src/main/java/rest/RestService.java
+++ Auction/src/main/java/rest/RestService.java
@@ -36,24 +36,14 @@ public class RestService {
 	Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create(); 
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
-	@Consumes(MediaType.APPLICATION_XML)
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	public List<Product> getAuctions() {
 		TypedQuery<Product> query = em.createNamedQuery(Product.FIND_ALL, Product.class);
 		return query.getResultList();
 	}
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String getAuctionsJSON() {
-		TypedQuery<Product> query = em.createNamedQuery(Product.FIND_ALL, Product.class);
-		return gson.toJson(query.getResultList());
-	}
 
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
-	@Consumes(MediaType.APPLICATION_XML)
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	@Path("{id}")
 	public Product getAuction(@PathParam("id") String id){
 		int idInt = Integer.parseInt(id);
@@ -63,22 +53,8 @@ public class RestService {
 		return product;
 	}
 	
-
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("{id}")
-	public String getAuctionJson(@PathParam("id") String id){
-		int idInt = Integer.parseInt(id);
-		Product product = em.find(Product.class, idInt);
-		if (product == null)
-			throw new NotFoundException();
-		return gson.toJson(product);
-	}
-	
-	@GET
-	@Produces(MediaType.APPLICATION_XML)
-	@Consumes(MediaType.APPLICATION_XML)
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	@Path("{id}/bids")
 	public List<Bid> getBids(@PathParam("id") String id) {
 		int idInt = Integer.parseInt(id);
@@ -89,20 +65,7 @@ public class RestService {
 	}
 	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("{id}/bids")
-	public String getBidsJSON(@PathParam("id") String id) {
-		int idInt = Integer.parseInt(id);
-		Product product = em.find(Product.class, idInt);
-		if (product == null)
-			throw new NotFoundException();
-		return gson.toJson(product.getBidHistory());
-	}
-	
-	@GET
-	@Produces(MediaType.APPLICATION_XML)
-	@Consumes(MediaType.APPLICATION_XML)
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	@Path("{id}/bids/{bid}")
 	public Bid getBid(@PathParam("id") String id, @PathParam("bid") String bId) {
 		int bIdInt = Integer.parseInt(bId);
@@ -112,21 +75,8 @@ public class RestService {
 		return bid;
 	}
 	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("{id}/bids/{bid}")
-	public String getBidJSON(@PathParam("id") String id, @PathParam("bid") String bId) {
-		int bIdInt = Integer.parseInt(bId);
-		Bid bid = em.find(Bid.class, bIdInt);
-		if (bid == null)
-			throw new NotFoundException();
-		return gson.toJson(bid);
-	}
-	
 	@POST
-	@Produces(MediaType.APPLICATION_XML)
-	@Consumes(MediaType.APPLICATION_XML)
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	@Path("{id}/bids")
 	public Bid postBid(@PathParam("id") String id, @QueryParam("amount") String amount){
 		int bidId = Integer.parseInt(id);
@@ -145,30 +95,6 @@ public class RestService {
 		return bid;
 			
 	}
-	
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("{id}/bids")
-	public String postBidJson(@PathParam("id") String id, @QueryParam("amount") String amount){
-		int bidId = Integer.parseInt(id);
-		int amountInt = Integer.parseInt(amount);
-
-		Product product = em.find(Product.class, bidId);
-		if (product == null)
-			throw new NotFoundException();
-		
-		
-		Bid bid = new Bid();
-		bid.setAmount(amountInt);
-		product.addBid(bid);
-		em.persist(bid);
-		
-		return gson.toJson(bid);
-			
-	}
-	
-	
 }
 
 

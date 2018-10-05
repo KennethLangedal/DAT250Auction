c1513b92676c8bd30db6c3b064f5d437c975dbd6
Markus <markus.loland@hotmail.com>
2018-10-05 16:11:54 +0200

    Rest Response

--- Auction/src/main/java/rest/RestService.java
+++ Auction/src/main/java/rest/RestService.java
@@ -6,7 +6,6 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
@@ -14,11 +13,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import javax.ws.rs.core.Response;
 
 import ejb.ProductEJB;
 import entities.Bid;
@@ -81,10 +77,24 @@ public class RestService {
 	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	@Path("{id}/bids")
-	public Bid postBid(@PathParam("id") String id, Bid bid){
+	public Response postBid(@PathParam("id") String id, Bid bid){
 		int productId = Integer.parseInt(id);
-		productEJB.placeBid(productId, 1, bid).ordinal();
-		return bid;
+		switch(productEJB.placeBid(productId, 3435, bid)) {
+		case LOW:
+			return Response.ok().build();
+		case MISSING_PRODUCT:
+	        return Response.status(404).entity("Missing product").build();
+		case MISSING_USER:
+			return Response.status(404).entity("Missing user").build();
+		case NOT_ACTIVE:
+			return Response.status(404).entity("not active").build();
+		case PRODUCT_CLOSED:
+			return Response.status(404).entity("product closed").build();
+			
+		};
+		return Response.ok(bid).build();
+	
+		
 	}
 }
 

ccaccd60f9adeb03dd93660838e2b4373f832592
Markus <markus.loland@hotmail.com>
2018-10-26 10:48:45 +0200

    null pointer fix

--- Auction/src/main/java/jms/DweetListener.java
+++ Auction/src/main/java/jms/DweetListener.java
@@ -15,7 +15,6 @@ import com.google.gson.JsonObject;
 import ejb.ProductEJB;
 import entities.Product;
 
-
 /**
  * @Author Markus
  * 
@@ -24,34 +23,46 @@ import entities.Product;
  */
 
 @MessageDriven(mappedName = "jms/dat250/Topic", activationConfig = {
-		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")})
+		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
 public class DweetListener implements MessageListener {
-	
+
 	@EJB
 	ProductEJB productEJB;
 
 	@Override
 	public void onMessage(Message message) {
-		
-		
 
 		try {
 			Product product = message.getBody(Product.class);
-			if(product.getEndTime().getTime() > System.currentTimeMillis())
+			if (product.getEndTime().getTime() > System.currentTimeMillis())
 				return;
 			Product pro = productEJB.findProduct(product.getId());
 			JsonObject json = new JsonObject();
 			json.addProperty("name", pro.getName());
-			json.addProperty("buyer", pro.getBuyer().toString());
-			json.addProperty("amount", pro.getLastBid().getAmount());
+			if (pro.getBuyer() != null) {
+				json.addProperty("buyer", pro.getBuyer().toString());
+				json.addProperty("amount", pro.getLastBid().getAmount());
+			} else {
+				json.addProperty("buyer", "no buyer");
+				json.addProperty("amount", "not sold");
+			}
+
 			json.addProperty("user", pro.getUser().toString());
+
 //			
-			
+
 			Logger logger = Logger.getLogger(getClass().getName());
-			logger.info("DTWEET name: " + pro.getName()); 
-			logger.info("DTWEET buyer: " + pro.getBuyer().toString()); 
-			logger.info("DTWEET amount: " +  pro.getLastBid().getAmount()); 
+			logger.info("DTWEET name: " + pro.getName());
+			if (pro.getBuyer() != null) {
+				logger.info("DTWEET buyer: " + pro.getBuyer().toString());
+				logger.info("DTWEET amount: " + pro.getLastBid().getAmount());
+			} else {
+				logger.info("DTWEET buyer: no buyer");
+				logger.info("DTWEET amount: not sold");
+			}
+
 			logger.info("DTWEET user: " + pro.getUser().toString());
+
 			logger.info("DTWEET: Sending bid to dweet...");
 			logger.info("DTWEET JSON: " + json);
 			try {
